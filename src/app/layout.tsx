87efcf5e1239615c2980b0a676d980913117166c
Guigui2912 <guillaume.demessence@hotmail.fr>
2026-03-16 11:16:50 +0100

    fix: script JS pour bloquer pull-to-refresh Android PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" style={{ overscrollBehavior: "none" }}>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function() {
+            var lastY = 0;
+            document.addEventListener('touchstart', function(e) {
+              lastY = e.touches[0].clientY;
+            }, { passive: true });
+            document.addEventListener('touchmove', function(e) {
+              var y = e.touches[0].clientY;
+              var el = e.target;
+              var scrollable = false;
+              while (el && el !== document.body) {
+                if (el.scrollTop > 0 || (el.scrollHeight > el.clientHeight && getComputedStyle(el).overflowY !== 'hidden')) {
+                  scrollable = true;
+                  break;
+                }
+                el = el.parentElement;
+              }
+              if (!scrollable && y > lastY) {
+                e.preventDefault();
+              }
+              lastY = y;
+            }, { passive: false });
+          })();
+        `}} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         style={{ overscrollBehavior: "none" }}
